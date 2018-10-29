b2e18a662434108003e8e366ba3e90e1e603d76a
Alex DS <alex0360@outlook.com>
2018-10-29 05:32:17 -0400

    Clases Genaricas

---
+++ Avanzado4_2ClasesGenericas/CPunto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avanzado4_2ClasesGenericas
+{
+    // Tipo Representa al tipo con el que se trabaja en un momento dado
+    class CPunto<Tipo>
+    {
+        // Creamos variables de tipo Tipo
+        private Tipo x;
+        private Tipo y;
+
+        // Podemos recibir parametros de tipo Tipo
+        public CPunto(Tipo px, Tipo py) {
+            x = px;
+            y = py;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("X={0}, Y={1}", x, y);
+        }
+
+        public void Reset() {
+            // Colocamos el valor de default para el tipo Tipo
+            // Numero 0
+            // Referencias Null
+
+            x = default(Tipo);
+            y = default(Tipo);
+        }
+
+        public void EncuentraTipo() {
+            if(typeof(Tipo) == typeof(int))
+                System.Console.WriteLine("Trabajando con Entero");
+            if(typeof(Tipo) == typeof(double))
+                System.Console.WriteLine("Trabajando con Doble");
+            if(typeof(Tipo) == typeof(string))
+                System.Console.WriteLine("Trabajando con Texto");
+            if(typeof(Tipo) == typeof(char))
+                System.Console.WriteLine("Trabajando con Caracter");
+            if(typeof(Tipo) == typeof(float))
+                System.Console.WriteLine("Trabajando con Flotante");
+        }
+    }
+}
---
+++ Avanzado4_2ClasesGenericas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Avanzado4_2ClasesGenericas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Para puntoI, T Trabaja como int
+            CPunto<int> puntoI = new CPunto<int>(3, 4);
+
+            // Para puntoI, T Trabaja como Double
+            CPunto<double> puntoD = new CPunto<double>(2.56, 1.87);
+
+            // Para puntoI, T Trabaja como Float
+            CPunto<float> puntoF = new CPunto<float>(8.98f, 7.69f);
+
+            Console.WriteLine(puntoI);
+            Console.WriteLine(puntoD);
+            Console.WriteLine(puntoF);
+
+            // Colocamos el valor de defaut
+            puntoI.Reset();
+            Console.WriteLine(puntoI);
+
+            // Verificar el tipo
+            puntoI.EncuentraTipo();
+            puntoF.EncuentraTipo();
+            puntoD.EncuentraTipo();
+
+            Console.ReadKey();
+        }
+    }
+}
